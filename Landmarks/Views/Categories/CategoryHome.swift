59f24641007fc068e8acb5e095b6ae3e629f24b1
FDC Wenman <139725768+FDCWenman@users.noreply.github.com>
2025-10-27 17:10:07 +0800

    Practice: Interfaces

---
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,39 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by FDC.Wenman-NC-SA-Web on 10/27/25.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    @Environment(ModelData.self) var modelData
+    
+    var body: some View {
+        NavigationSplitView {
+            List {
+                
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+                
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
+                }
+                .listRowInsets(EdgeInsets())
+            }
+            .navigationTitle("Featured")
+        } detail: {
+            Text("Select a landmark")
+        }
+    }
+}
+
+#Preview {
+    CategoryHome()
+        .environment(ModelData())
+}
