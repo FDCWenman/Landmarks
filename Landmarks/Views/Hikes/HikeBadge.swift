f477715c81362f64cd9fbe3f46b7cbaf4f8dee64
FDC Wenman <139725768+FDCWenman@users.noreply.github.com>
2025-10-27 17:36:30 +0800

    Practice: UI Controls + Profile2 chuchu

---
+++ Landmarks/Views/Hikes/HikeBadge.swift
@@ -0,0 +1,18 @@
+//
+//  HikeBadge.swift
+//  Landmarks
+//
+//  Created by FDC.Wenman-NC-SA-Web on 10/27/25.
+//
+
+import SwiftUI
+
+struct HikeBadge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HikeBadge()
+}
---
+++ Landmarks/Views/Profiles/ProfileEditor.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileEditor.swift
+//  Landmarks
+//
+//  Created by FDC.Wenman-NC-SA-Web on 10/27/25.
+//
+
+import SwiftUI
+
+struct ProfileEditor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileEditor()
+}
